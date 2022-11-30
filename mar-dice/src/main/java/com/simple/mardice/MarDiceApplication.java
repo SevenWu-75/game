e6ab6b5c1b1eac13bd6cfd98ff6aed76f3f0d55a
wangwubei <ancy0707@163.com>
2022-11-30 10:40:03 +0800

    init 初始化火星骰项目

---
+++ mar-dice/src/main/java/com/simple/mardice/MarDiceApplication.java
@@ -0,0 +1,13 @@
+package com.simple.mardice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarDiceApplication.class, args);
+    }
+
+}
---
+++ mar-dice/src/test/java/com/simple/mardice/MarDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.mardice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
