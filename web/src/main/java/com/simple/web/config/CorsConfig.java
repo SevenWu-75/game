a71bd5f983890dc76675abce58f854f510802ce2
wangwubei <ancy0707@163.com>
2023-08-11 16:40:03 +0800

    fix 暂时注释跨域配置

--- web/src/main/java/com/simple/web/config/CorsConfig.java
+++ web/src/main/java/com/simple/web/config/CorsConfig.java
@@ -1,19 +1,19 @@
-package com.simple.web.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600L);
-    }
-}
+//package com.simple.web.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:8080","http://172.16.20.4:8080")
+//                .allowedMethods("*")
+//                .allowedHeaders("Origin","Content-Type","Cookie","Accept","Token")
+//                .allowCredentials(true)
+//                .maxAge(3600L);
+//    }
+//}
